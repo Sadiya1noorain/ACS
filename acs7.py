2ea6ddcc5a08a3709b54c5b931970ad95f8f5dae
Sadiya Noorain <53908125+Sadiya1noorain@users.noreply.github.com>
2021-03-11 20:14:02 +0530

    Update acs7.py

--- acs7.py
+++ acs7.py
@@ -4,15 +4,17 @@ for i in range(3):
     print('Enter the name of student' + str(i + 1))
     stdnt = input()
     names.append(stdnt)
-    print('Enter marks of student ' +stdnt)
+    print('Enter marks(2 subjects) of ' +stdnt)
     score = input()
     marks.append(score)
 
-print('Names of all students\n',names)
-print('Marks of students respectively\n',marks)
+for i in range(3):
+    print('Name of student = ', names[i])
+    print('---> Marks = ', marks[i])
 
 SearchStudent = input('Enter name whose marks you want to access : ')
 for i in range(3):
     if (SearchStudent == names[i]):
         print('The marks of ', SearchStudent, 'are ', marks[i])
+    
     
