d7f88549b9e090b163f5c47e67ff398c493d71f0
Sadiya Noorain <53908125+Sadiya1noorain@users.noreply.github.com>
2021-03-11 18:38:52 +0530

    Update acs6.py

--- acs6.py
+++ acs6.py
@@ -3,8 +3,8 @@ unique = []
 list1 = input('Enter first list of countries ')
 list2 = input('Enter second list of countries ')
 
-list1 = list(list1.split(" "))
-list2 = list(list2.split(" "))
+list1 = list(list1.split())
+list2 = list(list2.split())
 m = len(list1)
 n = len(list2)
 list1 = list(set(list1))
