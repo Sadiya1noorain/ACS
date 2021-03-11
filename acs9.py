e48da493819f654a2af17925a9c5bb94b82ac733
Sadiya Noorain <53908125+Sadiya1noorain@users.noreply.github.com>
2021-03-12 00:49:14 +0530

    Update acs9.py

--- acs9.py
+++ acs9.py
@@ -4,6 +4,6 @@ b = 2
 print('The ceil of ', a, 'is : ', math.ceil(a))
 print('The floor of ', a, 'is : ', math.floor(a))
 print('The fabs of ', a, 'is : ', math.fabs(a))
-print('The pow of ', a, 'is : ', math.pow(a, 2))
+print('The value of ', a, 'raised to the power ', b, 'is : ', math.pow(a, b)) 
 print('The sqrt of ', a, 'is : ', math.sqrt(a))
-print('The exponential of ', a, 'is : ', math.exp(b))
+print('The exponential of ', b, 'is : ', math.exp(b))
